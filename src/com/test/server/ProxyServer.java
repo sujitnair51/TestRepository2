12da2f43950325d0839dda817cb4b98036749df0
XtremeGenie <sujitnair51@gmail.com>
2018-05-23 00:15:01 -0400

    refactoring the code

--- src/com/test/server/ProxyServer.java
+++ src/com/test/server/ProxyServer.java
@@ -2,6 +2,7 @@ package com.test.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 
@@ -14,36 +15,57 @@ public class ProxyServer implements Runnable {
 	// main method for proxy server
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		    
-
-        int port = 10001;	//default
-        
-        try {
-            serverSocket = new ServerSocket(port);
-            System.out.println("Proxy Server listening on: " + port);
-            
-        } catch (IOException e) {
-            System.out.println("IO Exception! "+e.getMessage());
-            System.exit(-1);
-        }
-        try {
-        	while (listening) {
-                new RequestHandler(serverSocket.accept()).start();
-            }
-        	serverSocket.close();
-        }catch(Exception e)
-        {
-        	System.out.println("Exception! "+e.getMessage());
-        }
-        finally {
-        	
-        }
+		        
+        ProxyServer proxyServer = new ProxyServer(10001);
+        proxyServer.listen();
         
     }
 	
+	public void run() {
+		
+		// do something
+	}
 	
 	
-	public void run() {}
-	// do something
+	public void listen(){
+
+		while(listening){
+			try {
+				Socket socket = serverSocket.accept();
+				Thread thread = new Thread(new RequestHandler(socket));
+				thread.start();	
+			} catch (SocketException e) {
+				System.out.println("Sever Socket Exception"+ e.getStackTrace());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public ProxyServer(int port) {
+
+			
+		new Thread(this).start();	
+
+		
+		try {
+			// Create the Server Socket for the Proxy Server
+			serverSocket = new ServerSocket(port);
+
+			System.out.println("Proxy Server listening on port " + serverSocket.getLocalPort() + "..");
+			listening = true;
+		} 
+
+		catch (SocketException se) {
+			System.out.println("Socket Exception"+ se.getStackTrace());
+			se.printStackTrace();
+		}
+		catch (SocketTimeoutException ste) {
+			System.out.println("Timeout Exception"+ ste.getStackTrace());
+		} 
+		catch (IOException io) {
+			System.out.println("IO exception "+ io.getStackTrace());
+		}
+	}
 
 }
