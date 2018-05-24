5dce7a8cc9a52c8a0625f723782830e544e9160f
XtremeGenie <sujitnair51@gmail.com>
2018-05-23 21:24:03 -0400

    added cache capability

--- src/com/test/server/ProxyServer.java
+++ src/com/test/server/ProxyServer.java
@@ -1,71 +1,110 @@
 package com.test.server;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class ProxyServer implements Runnable {
-	
-	static ServerSocket serverSocket;
-	
-	static volatile boolean listening = true;
-	
+
+	private ServerSocket serverSocket;
+
+	private volatile boolean listening = true;
+
+	// HashMap to hold the cached items
+	static HashMap<String, File> cache;
+
 	// main method for proxy server
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		        
-        ProxyServer proxyServer = new ProxyServer(10001);
-        proxyServer.listen();
-        
-    }
-	
-	public void run() {
-		
-		// do something
-	}
-	
-	
-	public void listen(){
 
-		while(listening){
+		ProxyServer proxyServer = new ProxyServer(8085);
+		proxyServer.listen();
+
+	}
+
+	public void run() {
+
+		Scanner scanner = new Scanner(System.in);
+
+		String cmd;
+		while (listening) {
+			cmd = scanner.nextLine();
+
+			if (cmd.equals("close")) {
+				listening = false;
+				closeServer();
+			}
+
+		}
+		scanner.close();
+	}
+
+	public void listen() {
+
+		while (listening) {
 			try {
 				Socket socket = serverSocket.accept();
 				Thread thread = new Thread(new RequestHandler(socket));
-				thread.start();	
+				thread.start();
 			} catch (SocketException e) {
-				System.out.println("Sever Socket Exception"+ e.getStackTrace());
+				System.out.println("Sever Socket Exception" + e.getStackTrace());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			// finally {
+			// closeServer();
+			// }
 		}
 	}
-	
+
 	public ProxyServer(int port) {
 
-			
-		new Thread(this).start();	
+		cache = new HashMap<>();
+		new Thread(this).start();
 
-		
 		try {
 			// Create the Server Socket for the Proxy Server
 			serverSocket = new ServerSocket(port);
 
 			System.out.println("Proxy Server listening on port " + serverSocket.getLocalPort() + "..");
 			listening = true;
-		} 
+		}
 
 		catch (SocketException se) {
-			System.out.println("Socket Exception"+ se.getStackTrace());
+			System.out.println("Socket Exception" + se.getStackTrace());
 			se.printStackTrace();
+		} catch (SocketTimeoutException ste) {
+			System.out.println("Timeout Exception" + ste.getStackTrace());
+		} catch (IOException io) {
+			System.out.println("IO exception " + io.getStackTrace());
 		}
-		catch (SocketTimeoutException ste) {
-			System.out.println("Timeout Exception"+ ste.getStackTrace());
-		} 
-		catch (IOException io) {
-			System.out.println("IO exception "+ io.getStackTrace());
+	}
+
+	public static File getCache(String url) {
+		return cache.get(url);
+	}
+
+	public static void setCache(String url, File file) {
+		cache.put(url, file);
+	}
+
+	private void closeServer() {
+		listening = false;
+
+		// Close Server Socket
+		try {
+			System.out.println("Terminating Connection");
+			serverSocket.close();
+		} catch (Exception e) {
+			System.out.println("Exception closing proxy's server socket");
+			e.printStackTrace();
 		}
+
 	}
 
 }
--- src/com/test/server/RequestHandler.java
+++ src/com/test/server/RequestHandler.java
@@ -1,108 +1,287 @@
 package com.test.server;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.StringTokenizer;
 
-public class RequestHandler extends Thread{
-	private Socket socket = null;
-    private static final int BUFFER_SIZE = 32768;
-    public RequestHandler(Socket socket) {
-        super("RequestHandler");
-        this.socket = socket;
-    }
+import javax.imageio.ImageIO;
 
-    public void run() {
-        
-    	try {
-            DataOutputStream out =
-		new DataOutputStream(socket.getOutputStream());
-            BufferedReader in = new BufferedReader(
-		new InputStreamReader(socket.getInputStream()));
+public class RequestHandler implements Runnable {
 
-            String inputLine, outputLine;
-            int cnt = 0;
-            String urlToCall = "";
-            
-            while ((inputLine = in.readLine()) != null) {
-                try {
-                    StringTokenizer strToken = new StringTokenizer(inputLine);
-                    strToken.nextToken();
-                } catch (Exception e) {
-                    break;
-                }
-                //parse the first line of the request to find the url
-                if (cnt == 0) {
-                    String[] tokens = inputLine.split(" ");
-                    urlToCall = tokens[1];
-                    //can redirect this to output log
-                    System.out.println("Request for : " + urlToCall);
-                }
+//  Client socket passed by proxy
+	Socket clientSocket;
 
-                cnt++;
-            }
-           
-            BufferedReader rd = null;
-            try {
-                
-                URL url = new URL(urlToCall);
-                URLConnection conn = url.openConnection();
-                conn.setDoInput(true);
-                
-                conn.setDoOutput(false);
-               
-               
-                InputStream is = null;
-                HttpURLConnection huc = (HttpURLConnection)conn;
-                if (conn.getContentLength() > 0) {
-                    try {
-                        is = conn.getInputStream();
-                        rd = new BufferedReader(new InputStreamReader(is));
-                    } catch (IOException ioe) {
-                        System.out.println("IO EXCEPTION " + ioe);
-                    }
-                }
-                
-                byte by[] = new byte[ BUFFER_SIZE ];
-                int index = is.read( by, 0, BUFFER_SIZE );
-                while ( index != -1 )
-                {
-                  out.write( by, 0, index );
-                  index = is.read( by, 0, BUFFER_SIZE );
-                }
-                out.flush();
+//	read data from client
+	BufferedReader reader;
 
-                
-            } catch (Exception e) {
-                
-                System.err.println("Exception! " + e);
-                
-                out.writeBytes("");
-            }
+//  write data to client
+	BufferedWriter writer;
 
-            
-            if (rd != null) {
-                rd.close();
-            }
-            if (out != null) {
-                out.close();
-            }
-            if (in != null) {
-                in.close();
-            }
-            if (socket != null) {
-                socket.close();
-            }
+//  initialize the client socket, reader and writer	
+	public RequestHandler(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+		try {
+			this.clientSocket.setSoTimeout(2000);
+			reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		}
+	}
+
+
+	@Override
+	public void run() {
+
+		// request string from client
+		String requestString;
+		try {
+			requestString = reader.readLine();
+			System.out.println(requestString);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Error reading request from client");
+			return;
+		}
+
+		// Parse out Request String URL
+
+		System.out.println("REQUEST STRING: " + requestString);
+		
+		// remove request type and space
+		String urlString = requestString.substring(requestString.indexOf(' ') + 1);
+
+		// Remove everything past next space
+		urlString = urlString.substring(0, urlString.indexOf(' '));
+
+		// Prepend http:// if necessary to create correct URL
+		if (!urlString.substring(0, 4).equals("http")) {
+			String temp = "http://";
+			urlString = temp + urlString;
+		}
+
+		// Check if we have a cached copy
+		File file;
+		if ((file = ProxyServer.getCache(urlString)) != null) {
+			System.out.println("Retrieving from Cache : " + urlString + "\n");
+			getCachedFile(file);
+		} else {
+			System.out.println("HTTP GET for : " + urlString + "\n");
+			addToCache(urlString);
+		}
+	}
+	
+	private void getCachedFile(File cachedFile) {
+		// Read from File containing cached web page
+		try {
+			// If file is an image write data to client using buffered image.
+			String fileExtension = cachedFile.getName().substring(cachedFile.getName().lastIndexOf('.'));
+
+			// Response that will be sent to the server
+			String response;
+			if ((fileExtension.contains(".png")) || fileExtension.contains(".jpg") || fileExtension.contains(".jpeg")
+					|| fileExtension.contains(".gif")) {
+				// Read in image from storage
+				BufferedImage image = ImageIO.read(cachedFile);
+
+				if (image == null) {
+					System.out.println("Image " + cachedFile.getName() + " was null");
+					response = "HTTP/1.0 404 NOT FOUND \n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
+					writer.write(response);
+					writer.flush();
+				} else {
+					response = "HTTP/1.0 200 OK\n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
+					writer.write(response);
+					writer.flush();
+					ImageIO.write(image, fileExtension.substring(1), clientSocket.getOutputStream());
+				}
+			}
+
+			// Standard text based file requested
+			else {
+				BufferedReader cachedFileBufferedReader = new BufferedReader(
+						new InputStreamReader(new FileInputStream(cachedFile)));
+
+				response = "HTTP/1.0 200 OK\n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
+				writer.write(response);
+				writer.flush();
+
+				String line;
+				while ((line = cachedFileBufferedReader.readLine()) != null) {
+					writer.write(line);
+				}
+				writer.flush();
+
+				// Close resources
+				if (cachedFileBufferedReader != null) {
+					cachedFileBufferedReader.close();
+				}
+			}
+
+			// Close Down Resources
+			if (writer != null) {
+				writer.close();
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error Sending Cached file to client");
+			e.printStackTrace();
+		}
+	}
+
+	
+	private void addToCache(String urlString) {
+
+		try {
+
+			// Compute a logical file name as per schema
+			// This allows the files on stored on disk to resemble that of the URL it was
+			// taken from
+			int fileExtensionIndex = urlString.lastIndexOf(".");
+			String fileExtension;
+
+			// Get the type of file
+			fileExtension = urlString.substring(fileExtensionIndex, urlString.length());
+
+			// Get the initial file name
+			String fileName = urlString.substring(0, fileExtensionIndex);
+
+			// Trim off http://www. as no need for it in file name
+			fileName = fileName.substring(fileName.indexOf('.') + 1);
+
+			// Remove any illegal characters from file name
+			fileName = fileName.replace("/", "__");
+			fileName = fileName.replace('.', '_');
+
+			// Trailing / result in index.html of that directory being fetched
+			if (fileExtension.contains("/")) {
+				fileExtension = fileExtension.replace("/", "__");
+				fileExtension = fileExtension.replace('.', '_');
+				fileExtension += ".html";
+			}
+
+			fileName = fileName + fileExtension;
+
+			// Attempt to create File to cache to
+			boolean caching = true;
+			File fileToCache = null;
+			BufferedWriter fileToCacheBW = null;
+
+			try {
+				// Create File to cache
+				fileToCache = new File("cached/" + fileName);
+
+				if (!fileToCache.exists()) {
+					fileToCache.createNewFile();
+				}
+
+				// Create Buffered output stream to write to cached copy of file
+				fileToCacheBW = new BufferedWriter(new FileWriter(fileToCache));
+			} catch (IOException e) {
+				System.out.println("Couldn't cache: " + fileName);
+				caching = false;
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				System.out.println("NPE opening file");
+			}
+
+			// Check if file is an image
+			if ((fileExtension.contains(".png")) || fileExtension.contains(".jpg") || fileExtension.contains(".jpeg")
+					|| fileExtension.contains(".gif")) {
+				// Create the URL
+				URL remoteURL = new URL(urlString);
+				BufferedImage image = ImageIO.read(remoteURL);
+
+				if (image != null) {
+					// Cache the image to disk
+					ImageIO.write(image, fileExtension.substring(1), fileToCache);
+
+					// Send response code to client
+					String line = "HTTP/1.0 200 OK\n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
+					writer.write(line);
+					writer.flush();
+
+					// Send them the image data
+					ImageIO.write(image, fileExtension.substring(1), clientSocket.getOutputStream());
+
+					// No image received from remote server
+				} else {
+					String error = "HTTP/1.0 404 NOT FOUND\n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
+					writer.write(error);
+					writer.flush();
+					return;
+				}
+			}
+
+			// File is a text file
+			else {
+
+				// Create the URL
+				URL remoteURL = new URL(urlString);
+				// Create a connection to remote server
+				HttpURLConnection proxyToServerCon = (HttpURLConnection) remoteURL.openConnection();
+				proxyToServerCon.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+				proxyToServerCon.setRequestProperty("Content-Language", "en-US");
+				proxyToServerCon.setUseCaches(false);
+				proxyToServerCon.setDoOutput(true);
+
+				// Create Buffered Reader from remote Server
+				BufferedReader proxyToServerBR = new BufferedReader(
+						new InputStreamReader(proxyToServerCon.getInputStream()));
+
+				// Send success code to client
+				String line = "HTTP/1.0 200 OK\n" + "Proxy-agent: ProxyServer/1.0\n" + "\r\n";
+				writer.write(line);
+
+				// Read from input stream between proxy and remote server
+				while ((line = proxyToServerBR.readLine()) != null) {
+					// Send on data to client
+					writer.write(line);
+
+					// Write to our cached copy of the file
+					if (caching) {
+						fileToCacheBW.write(line);
+					}
+				}
+
+				// Ensure all data is sent by this point
+				writer.flush();
+
+				// Close Down Resources
+				if (proxyToServerBR != null) {
+					proxyToServerBR.close();
+				}
+			}
+
+			if (caching) {
+				// Ensure data written and add to our cached hash maps
+				fileToCacheBW.flush();
+				ProxyServer.setCache(urlString, fileToCache);
+			}
+
+			// Close down resources
+			if (fileToCacheBW != null) {
+				fileToCacheBW.close();
+			}
+
+			if (writer != null) {
+				writer.close();
+			}
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
